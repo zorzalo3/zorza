6a840e7a908e1c1e855afc1a700b556abb7aa826
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2017-12-20 10:04:53 +0100

    Add empty urls and views for timetable

--- timetable/urls.py
+++ timetable/urls.py
@@ -5,4 +5,9 @@ from . import views
 app_name = 'timetable'
 urlpatterns = [
     path('', views.hello_world, name='hello'),
+    path('timetable/', views.show_default_timetable, name='timetable'),
+    path('timetable/class/<int>,', views.show_class_timetable, name='class_timetable'),
+    path('timetable/groups/<slug>,', views.show_groups_timetable, name='groups_timetable'),
+    path('timetable/room/<int>,', views.show_room_timetable, name='room_timetable'),
+    path('timetable/teacher/<int>,', views.show_teacher_timetable, name='teacher_timetable'),
 ]
--- timetable/views.py
+++ timetable/views.py
@@ -5,3 +5,18 @@ from django.shortcuts import render
 def hello_world(request):
     context = {}
     return render(request, 'timetable.html', context);
+
+def show_default_timetable(request):
+    pass
+
+def show_class_timetable(request):
+    pass
+
+def show_groups_timetable(request):
+    pass
+
+def show_room_timetable(request):
+    pass
+
+def show_teacher_timetable(request):
+    pass
