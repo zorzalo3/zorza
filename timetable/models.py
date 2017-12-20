cd7d3f7424a97a22983e85f8a8747305cab5d121
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2017-12-20 07:56:05 +0100

    Create blank timetable models

--- timetable/models.py
+++ timetable/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Class(models.Model):
+    pass
+
+class Group(models.Model):
+    pass
+
+class Lesson(models.Model):
+    pass
+
+class Subject(models.Model):
+    pass
+
+class Teacher(models.Model):
+    pass
+
+class Room(models.Model):
+    pass
+
+class Time(models.Model):
+    pass
