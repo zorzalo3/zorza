a9a7bdad43233c7bba70a29d38d859897618721f
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2017-12-28 19:00:21 +0100

    Create first working timetable
    Only shows subject names, lessons probably are in wrong columns.

--- timetable/views.py
+++ timetable/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from itertools import groupby
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from .models import *
 
 # Create your views here.
 
@@ -12,8 +15,23 @@ def show_default_timetable(request):
 def show_class_timetable(request):
     pass
 
-def show_groups_timetable(request):
-    pass
+def show_groups_timetable(request, group_ids):
+    try:
+        group_ids = {int(n) for n in group_ids.split(',')} # convert to set
+    except:
+        raise Http404
+    groups = [get_object_or_404(Group, pk=n) for n in group_ids]
+    lessons = Lesson.objects.filter(group__in=group_ids)
+    print(lessons)
+    days = {l.weekday: str(l.get_weekday_display()) for l in lessons}
+    lessons = sorted(lessons, key=lambda x: x.period)
+    periods = {k:g for k, g in groupby(lessons, key=lambda x: x.period)}
+    print(periods)
+    context = {
+        'days': days,
+        'periods': periods,
+    }
+    return render(request, 'timetable.html', context)
 
 def show_room_timetable(request):
     pass
