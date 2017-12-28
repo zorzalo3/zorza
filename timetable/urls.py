e0cf2e6c3e175da3bc7b58b10509d594f624b2fd
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2017-12-28 17:42:37 +0100

    Fix broken urls, move 'timetable/' prefix to zorza/urls.py

--- timetable/urls.py
+++ timetable/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 
 from . import views
 
-app_name = 'timetable'
 urlpatterns = [
-    path('', views.hello_world, name='hello'),
-    path('timetable/', views.show_default_timetable, name='timetable'),
-    path('timetable/class/<int>,', views.show_class_timetable, name='class_timetable'),
-    path('timetable/groups/<slug>,', views.show_groups_timetable, name='groups_timetable'),
-    path('timetable/room/<int>,', views.show_room_timetable, name='room_timetable'),
-    path('timetable/teacher/<int>,', views.show_teacher_timetable, name='teacher_timetable'),
+    path('', views.show_default_timetable, name='timetable'),
+    path('class/<int:class_id>/', views.show_class_timetable, name='class_timetable'),
+    path('groups/<group_ids>/', views.show_groups_timetable, name='groups_timetable'),
+    path('room/<int:room_id>/', views.show_room_timetable, name='room_timetable'),
+    path('teacher/<int:teacher_id>/', views.show_teacher_timetable, name='teacher_timetable'),
 ]
--- zorza/urls.py
+++ zorza/urls.py
@@ -19,6 +19,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('timetable.urls')),
+    path('timetable/', include('timetable.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
