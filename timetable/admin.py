98bfc0fa15aff1bddf388069f6fe52d9c56815ed
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2017-12-28 17:46:46 +0100

    Register all timetable models in admin.py

--- timetable/admin.py
+++ timetable/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Class)
+admin.site.register(Group)
+admin.site.register(Subject)
+admin.site.register(Teacher)
+admin.site.register(Room)
+admin.site.register(Times)
+admin.site.register(Period)
+admin.site.register(DayPlan)
+admin.site.register(Lesson)
