22976d98d6014a51949c58e0b0dcbc68e678f204
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-01-06 02:02:33 +0100

    Fix period representation not showing

--- timetable/utils.py
+++ timetable/utils.py
@@ -3,10 +3,10 @@ from .models import *
 
 days = settings.TIMETABLE_WEEKDAYS
 
-# Not always all those values are needed. 
+# Not always all those values are needed.
 # Move to views.py with values for each view?
 lesson_values = [
-    'id', 'group_id', 'group__name', 'subject_id', 'subject__name', 
+    'id', 'group_id', 'group__name', 'subject_id', 'subject__name',
     'subject__short_name', 'teacher_id', 'teacher__name', 'teacher__initials',
     'period__number', 'weekday', 'room_id', 'room__name', 'room__short_name',
 ]
@@ -15,11 +15,13 @@ def get_timetable_context(lessons):
     lessons = lessons.values(*lesson_values)
     periods = Period.objects.all()
     # TODO: implement DayPlan
-    
-    table = {period.number: {day[0]: [] for day in days} for period in periods}
+
+    # TODO: a cleaner way to pass str(period) to the template while using period.number as key?
+    table = {period.number: [str(period), {day[0]: [] for day in days}] for period in periods}
+
     for lesson in lessons:
         # Will throw exception if lesson.weekday not in days
-        table[lesson['period__number']][lesson['weekday']].append(lesson)
+        table[lesson['period__number']][1][lesson['weekday']].append(lesson)
 
     context = {
         'days': days,
