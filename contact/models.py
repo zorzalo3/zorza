f0716cf4f20c1b22ac55353b0baf7bd5fa9d114a
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-03-05 19:21:01 +0100

    Make the contact form mail admins on send

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import mail_admins
 
 class Message(models.Model):
     sender = models.EmailField(_('Your email address'))
@@ -9,3 +10,11 @@ class Message(models.Model):
 
     def __str__(self):
         return self.subject
+
+    def save(self, *args, **kwargs):
+        # If creating a new instance of Message
+        if self.pk is None:
+            message = '%s: \n\n%s' % (self.sender, self.content)
+            mail_admins(self.subject, message)
+        super().save(*args, **kwargs)
+
