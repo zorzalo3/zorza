3c42d819de7186427574199b0127b84a74fd264b
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-05-16 22:57:09 +0200

    Show meaningful None option in substitution form
    'cancelled' instead of '------'

--- timetable/forms.py
+++ timetable/forms.py
@@ -10,8 +10,17 @@ class SelectTeacherAndDateForm(Form):
     teacher = ModelChoiceField(label=_('Teacher'), queryset=Teacher.objects.all())
     date = DateField(label=_('Date'), initial=get_next_schoolday)
 
+class SubstitutionForm(ModelForm):
+    class Meta:
+        model = Substitution
+        fields = ('period', 'substitute',)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['substitute'].empty_label = _('cancelled')
+
 SubstitutionFormSet = modelformset_factory(
-    Substitution, fields=('period', 'substitute'), extra=8, can_delete=True)
+    Substitution, form=SubstitutionForm, extra=8, can_delete=True)
+
 
 DayPlanFormSet = modelformset_factory(
     DayPlan, fields='__all__', extra=8)
