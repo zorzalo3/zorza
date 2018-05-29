14491f40038c479dbef96d64236646a93f233b66
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-05-29 14:43:58 +0200

    Never cache /substitutions/add/

--- timetable/urls.py
+++ timetable/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from .views import *
 
@@ -10,7 +11,7 @@ urlpatterns = [
     path('teacher/<int:teacher_id>/', show_teacher_timetable, name='teacher_timetable'),
     path('personalize/<int:class_id>/', personalize, name='personalize'),
     path('schedules/', show_schedules, name='schedules'),
-    path('substitutions/add/', AddSubstitutionsView1.as_view(), name='add_substitutions1'),
+    path('substitutions/add/', never_cache(AddSubstitutionsView1.as_view()), name='add_substitutions1'),
     path('substitutions/add/<int:teacher_id>/<date>/', add_substitutions2, name='add_substitutions2'),
     path('manage/', manage, name='manage'),
     path('calendar/edit/', edit_calendar, name='edit_calendar'),
