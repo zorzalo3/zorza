ada3076ae997c97ee3f8c38466b51df1a89cf007
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-06-05 14:16:40 +0200

    Add a 'my_documents' page

--- documents/urls.py
+++ documents/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.show_category, name='documents'),
     path('category/<int:parent_id>/', views.show_category, name='category'),
     path('<int:document_id>/', views.show_document, name='document'),
+    path('mine/', views.show_mine, name='my_documents'),
 ]
--- documents/views.py
+++ documents/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import *
 
 def show_category(request, parent_id=None):
@@ -21,3 +22,11 @@ def show_document(request, document_id):
         'document': document,
     }
     return render(request, 'show_document.html', context)
+
+@login_required
+def show_mine(request):
+    items = Item.objects.filter(author=request.user).select_related('category')
+    context = {
+        'items': items,
+    }
+    return render(request, 'show_mine.html', context)
