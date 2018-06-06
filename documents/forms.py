8dae20f4d74a3bf990cb3dae3e5e1c5302010537
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-06-06 08:11:07 +0200

    Add PL translation for document field labels

--- documents/forms.py
+++ documents/forms.py
@@ -4,13 +4,22 @@ from django.utils.translation import gettext_lazy as _
 from .models import *
 
 class FileForm(ModelForm):
-    category = ModelChoiceField(queryset=Category.objects.all(), required=False)
+    category = ModelChoiceField(label=_('Category'), queryset=Category.objects.all(), required=False)
     class Meta:
         model = File
         fields = ('title', 'category', 'data')
+        labels = {
+            'title': _('Title'),
+            'data': _('File'),
+        }
 
 class DocumentForm(ModelForm):
-    category = ModelChoiceField(queryset=Category.objects.all(), required=False)
+    category = ModelChoiceField(label=_('Category'), queryset=Category.objects.all(), required=False)
     class Meta:
         model = Document
         fields = ('title', 'category', 'content')
+        labels = {
+            'title': _('Title'),
+            'content': _('Content'),
+        }
+
