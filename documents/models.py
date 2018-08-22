162e58e36a6cec7526ebca5dca81770f6743be7e
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-08-22 14:13:46 +0200

    Fix Category and Class plurals

--- documents/models.py
+++ documents/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'categories'
+
 class Item(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
                                blank=True, on_delete=models.SET_NULL)
--- timetable/models.py
+++ timetable/models.py
@@ -13,6 +13,9 @@ class Class(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'classes'
+
 class Group(models.Model):
     name = models.CharField(max_length=15)
     classes = models.ManyToManyField(Class, blank=True)
