fb3d0bbbd10e5115fccda7dac8cd887504e4986c
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2018-09-11 15:01:30 +0200

    Remove tinymce.urls from zorza/urls.py

--- zorza/urls.py
+++ zorza/urls.py
@@ -27,7 +27,6 @@ urlpatterns = [
     path('contact/', include('contact.urls')),
     path('admin/', admin.site.urls),
     path('documents/', include('documents.urls')),
-    path('tinymce/', include('tinymce.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
