295986e017d4ae239f8cd67f0a6c2ac65d59b4fa
Wiktor Kuchta <wiktorkuchta@protonmail.com>
2019-09-13 12:14:32 +0200

    Add basic Category ordering test

--- documents/tests.py
+++ documents/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import *
 
-# Create your tests here.
+class CategoryOrderTest(TestCase):
+
+    def setUp(self):
+        self.category = Category(name='cat')
+        self.category.save()
+        obj1 = Document(title='abcdef', category=self.category)
+        obj1.save()
+        obj2 = Document(title='zxcvbn', category=self.category)
+        obj2.save()
+
+    def test_title_ordering(self):
+        self.category.order = 'title'
+        self.category.save()
+        c = Client()
+        response = c.get('/documents/category/' + str(self.category.pk) + '/')
+        items = list(response.context['items'])
+        self.assertEquals(len(items), 2)
+        self.assertTrue(items[0].title < items[1].title)
+
+        self.category.order = '-title'
+        self.category.save()
+        response = c.get('/documents/category/' + str(self.category.pk) + '/')
+        items = list(response.context['items'])
+        self.assertEquals(len(items), 2)
+        self.assertTrue(items[0].title > items[1].title)
